ce47190c91385a5dc795f3304330ccf449e7cf6a
Mingwei Hu <calvinmwhu@gmail.com>
2015-08-26 13:16:47 -0500

    first commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,33 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to @movie
+  end
+
+private
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :released_on, :total_gross)
+  end
+end
